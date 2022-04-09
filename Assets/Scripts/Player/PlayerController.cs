f3a20e93a8670c643d7a27188f7b5b0fbbab5eb7
Alexandre COULON <alex97.coul@gmail.com>
2022-04-09 11:07:29 +0200

    Switching to new input system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        private CharacterController _controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region Variables: Movement
+        private Vector3 _move;
+        [SerializeField]
+        private float _MOVE_SPEED = 15f;
+        #endregion
+
+        #region Variables: Inputs
+        private DefaultInputActions _inputActions;
+        private InputAction _moveAction;
+        #endregion
+
+        void Awake()
+        {
+            _inputActions = new DefaultInputActions();
+            _controller = GetComponent<CharacterController>();
+        }
+
+        void OnEnable()
+        {
+            _moveAction = _inputActions.Player.Move;
+            _moveAction.Enable();
+        }
+
+        void OnDisable()
+        {
+            _moveAction.Disable();
+        }
+
+        void Update()
+        {
+            _move = _moveAction.ReadValue<Vector2>();
+            _controller.Move(
+                new Vector3(_move.x, 0f, _move.y) * 
+                Time.deltaTime * 
+                _MOVE_SPEED
+            );
+        }
     }
 }
